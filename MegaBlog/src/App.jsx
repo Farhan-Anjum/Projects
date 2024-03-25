1e295bdaef188326582e486ba116fad677df7dcf
Farhan Anjum <your.email@example.com>
2024-03-25 19:34:20 +0500

    Configure the redux for project.

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,13 +1,37 @@
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import './App.css';
+import AuthService from './appwrite/auth';
+import {login, logout} from './store/authSlice';
+import {Footer, Header} from './components';
 
-function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+function App () {
+  const [loading, setLoading] = useState (true);
+  const dispatch = useDispatch ();
 
-  return (
-    <>
-      <h1>MegaBlog with Appwrite</h1>
-    </>
-  )
+  useEffect (() => {
+    AuthService.getCurrentUser ()
+      .then (userData => {
+        if (userData) {
+          dispatch (login ({userData}));
+        } else {
+          dispatch (logout ());
+        }
+      })
+      .finally (() => setLoading (false));
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  //Outlet
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
